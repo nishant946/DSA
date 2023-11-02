eaece5547d52458f2a89bdeafbe90585f82f0798
nishat946 <nishantkumarsingh946@gmail.com>
2023-11-02 13:13:00 +0530

    commit

---
+++ endl.c
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    unsigned int i= 1;
+    char *c = (char*)&i;
+    if(*c != 0){
+        cout<<"little endian\n";
+    }
+    else{
+        cout<<"big endian\n";
+    }
+    return 0;
+}
